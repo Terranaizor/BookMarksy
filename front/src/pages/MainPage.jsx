429169d13d677c6ee619d0897526dd912bc579f8
Victory-S29 <viktoriia.sanina.2917@gmail.com>
2024-09-24 17:17:24 +0200

    feat: Implemented navigation element, banner with design. Created basic SCSS mixins

---
+++ front/src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import BlurNotifications from '../components/BlurNotifications';
+
+const MainPage = () => {
+    return (
+        <div>
+            <NavBar />
+            <BlurNotifications />
+        </div>
+    );
+};
+
+export default MainPage;
