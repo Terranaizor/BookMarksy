546342158499e4c3f7708030d6ebf76ebbdb8443
Terranaizor <radinnazar@gmail.com>
2024-10-26 13:40:53 +0300

    started work on auth

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework.views import Response, status
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
+class LoginAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': str(token), 'user' : UserSerializer(user).data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid username or password'}, status=status.HTTP_400_BAD_REQUEST)
