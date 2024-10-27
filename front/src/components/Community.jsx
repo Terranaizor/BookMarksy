51f623959234662cd724e47ed49103154b229dd8
Victory-S29 <viktoriia.sanina.2917@gmail.com>
2024-10-27 15:59:56 +0100

    fixed small problems and warnings

--- front/src/components/Community.jsx
+++ front/src/components/Community.jsx
@@ -22,7 +22,7 @@ const Community = () => {
             <h2>Join Community</h2>
             <section className='community-grid'>
                 {communityAdvantages.map((item, index) => (
-                    <div className='community-grid--element'>
+                    <div className='community-grid--element' key={index}>
                         <img src={item.imageSrc} alt={item.label} />
                         <p>{item.label}</p>
                     </div>
