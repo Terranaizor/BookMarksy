cc2ccf207bfe1052e1082680d101f69a8b462778
Nazar Radin <nradin@c2r4s8.42wolfsburg.de>
2024-11-11 16:02:09 +0100

    work on problems with backups

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# /app/wait-for-it.sh db:5432 --timeout=30 --strict --
+
 # Run migrations and load data
 python3 manage.py migrate --noinput
 python3 manage.py loaddata /app/backup/data_backup.json
@@ -7,9 +9,9 @@ python3 manage.py loaddata /app/backup/data_backup.json
 # Trap the SIGTERM signal to create the backup before the container stops
 
 shutdown() {
-  echo "Saving data backup..."
-  python3 manage.py dumpdata --output=/app/backup/data_backup.json
-  echo "Backup saved. Shutting down..."
+  # if python3 manage.py dbshell -c "SELECT 1" &> /dev/null; then
+    python3 manage.py dumpdata --output=/app/backup/data_backup.json
+    # fi
 }
 
 # Set up the trap for SIGTERM and SIGINT (for graceful shutdown)
